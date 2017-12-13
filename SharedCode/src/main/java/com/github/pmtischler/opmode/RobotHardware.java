e39fc5a7b7a315389c773e9b098034f7b519be5b
Phillip Tischler <pmtischler@gmail.com>
2017-12-13 07:09:09 -0500

    Fix misassignment of Mecanum out to motors.

--- SharedCode/src/main/java/com/github/pmtischler/opmode/RobotHardware.java
+++ SharedCode/src/main/java/com/github/pmtischler/opmode/RobotHardware.java
@@ -66,8 +66,8 @@ public abstract class RobotHardware extends OpMode {
     protected void setDriveForMecanum(Mecanum.Motion motion) {
         Mecanum.Wheels wheels = Mecanum.motionToWheels(motion);
         setPower(MotorName.DRIVE_FRONT_LEFT, wheels.frontLeft);
-        setPower(MotorName.DRIVE_BACK_LEFT, wheels.frontRight);
-        setPower(MotorName.DRIVE_FRONT_RIGHT, wheels.backLeft);
+        setPower(MotorName.DRIVE_FRONT_RIGHT, wheels.frontRight);
+        setPower(MotorName.DRIVE_BACK_LEFT, wheels.backLeft);
         setPower(MotorName.DRIVE_BACK_RIGHT, wheels.backRight);
     }
 
@@ -80,8 +80,8 @@ public abstract class RobotHardware extends OpMode {
         Mecanum.Wheels wheels = Mecanum.motionToWheels(motion).scaleWheelPower(
                 Math.sqrt(2));
         setPower(MotorName.DRIVE_FRONT_LEFT, wheels.frontLeft);
-        setPower(MotorName.DRIVE_BACK_LEFT, wheels.frontRight);
-        setPower(MotorName.DRIVE_FRONT_RIGHT, wheels.backLeft);
+        setPower(MotorName.DRIVE_FRONT_RIGHT, wheels.frontRight);
+        setPower(MotorName.DRIVE_BACK_LEFT, wheels.backLeft);
         setPower(MotorName.DRIVE_BACK_RIGHT, wheels.backRight);
     }
 
